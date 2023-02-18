94f70ccc94b226762c47379974a035512a042b59
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2023-02-18 23:31:34 +0530

    add body parser

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   console.log("Get api Call");
